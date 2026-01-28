485007c05666e323fde7c0f2505947c0abaaebd2
Saanvi <saanvi.ravikiran@gmail.com>
2026-01-28 16:03:45 +0530

    Pin python version to 3.11 for cryptography

---
+++ db.py
@@ -0,0 +1,10 @@
+from pymongo import MongoClient
+import os
+
+MONGO_URI = os.getenv("MONGO_URI")
+
+client = MongoClient(MONGO_URI)
+db = client["linkedin_agent"]
+
+users_collection = db["users"]
+
